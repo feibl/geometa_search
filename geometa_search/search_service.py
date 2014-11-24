66b97bc850414d6bf7fdc7a987c794eab40dbf2f
Fabian Senn <fabian.senn@gmail.com>
2014-11-24 14:37:07 +0100

    parameter renamed

--- geometa_search/search_service.py
+++ geometa_search/search_service.py
@@ -3,13 +3,13 @@ class SearchService(object):
     Declares functions used for searching records
     """
 
-    def search_by_keyword(query_string, start_index=0, how_many=10):
+    def search_by_keyword(query_string, start_index=0, max_records=10):
         """
         Searches records matching the given keywords.
 
         :param query_string: the query
         :param start_index: starting index in the result list
-        :param how_many: maximum numbers of records returned
+        :param max_records: maximum numbers of records returned
         """
         raise NotImplementedError()
 
