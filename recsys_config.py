964949ba7b4b197842d791b3263ae59ae55b0155
Fabian Senn <fabian.senn@gmail.com>
2015-01-27 12:26:52 +0100

    added DB boilerplate

--- recsys_config.py
+++ recsys_config.py
@@ -12,6 +12,15 @@ class Config(object):
     API_KEY = 'a18cccd4ff6cd3a54a73529e2145fd36'
 
 
+class HerokuConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']\
+        if 'DATABASE_URL' in os.environ else\
+        'postgresql://postgres:postgres@localhost/search_rex'
+    API_KEY = 'a18cccd4ff6cd3a54a73529e2145fd36'
+
+
 class ProductionConfig(Config):
     pass
 
