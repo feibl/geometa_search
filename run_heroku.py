55a5b736fece2fe824ff0f491e4a7af8162b38b8
Fabian Senn <fabian.senn@gmail.com>
2015-01-27 11:35:32 +0100

    Added run file for heroku

---
+++ run_heroku.py
@@ -0,0 +1,20 @@
+from geometa_search.factory import create_app as create_frontend
+from search_rex.factory import create_app as create_backend
+from search_rex.recommendations import create_recommender_system
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
+
+backend = create_backend('recsys_config.DevelopmentConfig')
+create_recommender_system(backend)
+frontend = create_frontend('config.DevelopmentConfig')
+app = DispatcherMiddleware(
+    frontend, {
+        '/recommender': backend
+    }
+)
+
+if __name__ == '__main__':
+    run_simple(
+        'localhost', 33507, app, use_reloader=True,
+        use_debugger=True, threaded=True)
